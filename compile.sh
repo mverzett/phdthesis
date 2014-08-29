8b6d3999e1af786ee7560e0e11d03496d08a5ce9
Mauro Verzetti <mverzett@cern.ch>
2014-08-29 18:35:38 +0200

    better version

--- compile.sh
+++ compile.sh
@@ -1,7 +1,15 @@
 #! /bin/bash
 
-rm *.aux *.bbl *.blg *.brf *.log *.out *.synctex.gz main.toc
-pdflatex main.tex
-bibtex main.aux 
-pdflatex main.tex
-pdflatex main.tex
+if [ "$1" = "fast" ]; then
+    pdflatex main.tex    
+elif [ "$1" = "bib" ]; then
+    bibtex main.aux 
+    pdflatex main.tex    
+else
+    rm *.aux *.bbl *.blg *.brf *.log *.out *.synctex.gz main.toc
+    pdflatex main.tex
+    bibtex main.aux 
+    pdflatex main.tex
+    pdflatex main.tex
+fi
+
